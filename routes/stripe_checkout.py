8f9b0f3ed7c36ee95031730689edbd422f74b663
Ricardo d'Avila <ricardodavilaoficial@gmail.com>
2025-09-22 18:40:59 -0300

    checkout: success/cancel -> web.app (temporário)

--- routes/stripe_checkout.py
+++ routes/stripe_checkout.py
@@ -33,8 +33,8 @@ def api_stripe_checkout():
         stripe.api_key = _get_secret_key()
 
         # Sucesso/cancelamento (absolutos)
-        success_url = _abs_url("https://www.meirobo.com.br/")
-        cancel_url  = _abs_url("https://www.meirobo.com.br/")
+        success_url = _abs_url("https://mei-robo-prod.web.app/")
+        cancel_url  = _abs_url("https://mei-robo-prod.web.app/")
 
         # Produto/Preço da assinatura/ativação:
         # Opção A (rápida p/ teste): define line_items com price_data inline
