c5903116a199b1fd3dfca010687960b492cf980f
Ricardo d'Avila <ricardodavilaoficial@gmail.com>
2025-09-22 19:24:33 -0300

    checkout: success/cancel absolutos (sem _abs_url) -> backend Render

--- routes/stripe_checkout.py
+++ routes/stripe_checkout.py
@@ -33,8 +33,8 @@ def api_stripe_checkout():
         stripe.api_key = _get_secret_key()
 
         # Sucesso/cancelamento (absolutos)
-        success_url = _abs_url("https://mei-robo-prod.web.app/")
-        cancel_url  = _abs_url("https://mei-robo-prod.web.app/")
+        success_url = _abs_url("https://mei-robo-prod.onrender.com/")
+        cancel_url  = _abs_url("https://mei-robo-prod.onrender.com/")
 
         # Produto/Preço da assinatura/ativação:
         # Opção A (rápida p/ teste): define line_items com price_data inline
